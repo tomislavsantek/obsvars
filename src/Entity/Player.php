08e11ffdcc353c5ab150c64f67545ad890a16872
Tomislav Santek <tomislav.santek@i-ways.hr>
2019-09-21 10:46:58 +0200

    Added entities

---
+++ src/Entity/Player.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PlayerRepository")
+ */
+class Player
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Round", mappedBy="Player1")
+     */
+    private $rounds;
+
+    public function __construct()
+    {
+        $this->rounds = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Round[]
+     */
+    public function getRounds(): Collection
+    {
+        return $this->rounds;
+    }
+
+    public function addRound(Round $round): self
+    {
+        if (!$this->rounds->contains($round)) {
+            $this->rounds[] = $round;
+            $round->setPlayer1($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRound(Round $round): self
+    {
+        if ($this->rounds->contains($round)) {
+            $this->rounds->removeElement($round);
+            // set the owning side to null (unless already changed)
+            if ($round->getPlayer1() === $this) {
+                $round->setPlayer1(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Round.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\RoundRepository")
+ */
+class Round
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Player", inversedBy="rounds")
+     */
+    private $Player1;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Player")
+     */
+    private $Player2;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $Player1Score;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $Player2Score;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPlayer1(): ?Player
+    {
+        return $this->Player1;
+    }
+
+    public function setPlayer1(?Player $Player1): self
+    {
+        $this->Player1 = $Player1;
+
+        return $this;
+    }
+
+    public function getPlayer2(): ?Player
+    {
+        return $this->Player2;
+    }
+
+    public function setPlayer2(?Player $Player2): self
+    {
+        $this->Player2 = $Player2;
+
+        return $this;
+    }
+
+    public function getPlayer1Score(): ?int
+    {
+        return $this->Player1Score;
+    }
+
+    public function setPlayer1Score(int $Player1Score): self
+    {
+        $this->Player1Score = $Player1Score;
+
+        return $this;
+    }
+
+    public function getPlayer2Score(): ?int
+    {
+        return $this->Player2Score;
+    }
+
+    public function setPlayer2Score(int $Player2Score): self
+    {
+        $this->Player2Score = $Player2Score;
+
+        return $this;
+    }
+}
